53fb689c3414a455545c7ffe96ba8a23a87c8061
rasika-chavan <58884494+rasika-chavan@users.noreply.github.com>
2020-06-15 20:01:36 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from csvisual.com/akroot-html/Services.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 09 May 2020 07:19:47 GMT -->
+<?php include "files/header.php" ?>
+		<div id="main-content" class="active">
+			<!-- Section Breadcrumb 1 -->
+		    <div id="section-breadcrumb1" class="bg2">
+		    	<div class="container">
+		    		<div class="row">
+		    			<div class="content col-12">
+		    				<h1>Our Services</h1>
+		    				<ul>
+		    					<li><a href="#">HOME</a></li>
+		    					<li class="current"><a href="#">OUR SERVICES</a></li>
+		    				</ul>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+			<!-- /.Section Breadcrumb 1 -->
+			<!-- Section Services 1 -->
+			<div id="section-services1">
+				<div class="container">
+					<div class="row">
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-like"></i>
+							<h3>Beauty Services</h3>
+							<p>Facial, Manicure, pedicure and many more</p>
+						</div>
+						<!-- /.Item -->
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-layers"></i>
+							<h3>Event Panning</h3>
+							<p>Marriages,Birthday and Many more.</p>
+						</div>
+						<!-- /.Item -->
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-fingerprint"></i>
+							<h3>Work</h3>
+							<p>Embroidery work,Maggam work,Pin work,Block printing</p>
+						</div>
+						<!-- /.Item -->
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-worldwide"></i>
+							<h3>Designing</h3>
+							<p>Blouse designing.</p>
+						</div>
+						<!-- /.Item -->
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-diploma"></i>
+							<h3>Clothing</h3>
+							<p>Lehanga stitching,Long frocks,Patiyala,Ruffle gaghras,Chudidars,Saree Rolling.</p>
+						</div>
+						<!-- /.Item -->
+						<!-- Item -->
+						<div class="item col-sm-12 col-md-4">
+							<i class="flaticon-help"></i>
+							<h3>Support 24/7</h3>
+
+						</div>
+                       
+						<!-- /.Item -->
+					</div>
+				</div>
+			</div>
+			<!-- /.Section Services 1 -->
+			<!-- Section Services 2 -->
+		
+			<!-- /.Section Services 2 -->
+			<!-- Section Services 2 -->
+			<style>
+            .button {
+   /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+			<!-- /.Section How We Work -->
+			<!-- Section CTA 3 -->
+            <div id="section-cta3">
+				<div class="container">
+					<div class="row">
+						<div class="title1 col-12">
+							<h2><span>Click <a href="portfolio.php"><button class="button" style="background-color: #555555">Here</button>For some of our works</a></h2>
+							<h2><span>Click <a href="https://www.raginidesigner.me/beauty.php"><button class="button" style="background-color: #555555">Here</button>For Beauty Services</a></h2>
+							<h2><span>Click <a href="https://raginidesigner.me/services-work.php"><button class="button" style="background-color: #555555">Here</button>For Designing Services</a></h2>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div id="section-cta3">
+				<div class="container">
+					<div class="row">
+						<div class="title1 col-12">
+							<h2><span>RaginiDesign is the most impressive team you were</span> Looking For</h2>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="container">
+		<!-- Section: Testimonials v.3 -->
+
+<!-- Section: Testimonials v.3 -->
+			<!-- /.Section CTA 3 -->
+			<!-- Section Footer -->
+            <?php include "files/footer.php" ?>
+			<!-- /.Section Footer -->
+		</div>
+	</div>
+
+	<!-- Javascript Files -->
+	<script src="assets/js/jquery2.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Swiper Slider -->
+	<script src="assets/js/swiper.min.js"></script>
+	<!-- OWL Carousel -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- Waypoint -->
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<!-- Easy Waypoint -->
+	<script src="assets/js/easy-waypoint-animate.js"></script>
+	<!-- Scripts -->
+	<script src="assets/js/scripts.js"></script>
+
+</body>
+
+<!-- Mirrored from csvisual.com/akroot-html/Services.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 09 May 2020 07:20:37 GMT -->
+</html>
